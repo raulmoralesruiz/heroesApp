89412768425d14d3b83d68dec424edf3bbbc3bf6
Raúl Morales <raulmdaw@gmail.com>
2024-01-20 15:28:48 +0100

    196. HeroesService - Traer información de héroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,8 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
+
     AppRoutingModule,
     SharedModule,
   ],
