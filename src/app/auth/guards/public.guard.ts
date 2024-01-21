6a2584b68f6c121ab03d48b49d2b722097149e36
Raúl Morales <raulmdaw@gmail.com>
2024-01-22 00:06:56 +0100

    227. PublicGuard - Tarea

---
+++ src/app/auth/guards/public.guard.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, CanMatch, Route, UrlSegment, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable, map, tap } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({ providedIn: 'root' })
+export class PublicGuard implements CanMatch, CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  private checkAuthStatus(): boolean | Observable<boolean> {
+    return this.authService.checkAuthentication().pipe(
+      tap((isAuthenticated) => {
+        console.log('Authenticated:', isAuthenticated)
+      }),
+      tap((isAuthenticated) => {
+        if (isAuthenticated) this.router.navigateByUrl('/');
+      }),
+      map( isAuthenticated => !isAuthenticated )
+    );
+  }
+
+  canMatch(
+    route: Route,
+    segments: UrlSegment[]
+  ): boolean | Observable<boolean> {
+    // console.log('canMatch');
+    // console.log({route, segments})
+
+    return this.checkAuthStatus();
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean | Observable<boolean> {
+    // console.log('canActivate');
+    // console.log({route, state})
+
+    return this.checkAuthStatus();
+  }
+}
